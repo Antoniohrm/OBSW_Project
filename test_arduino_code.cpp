a47aa44fd69b45a981d304e170fa5f83b8a07b98
Antonio Hernández-Rodicio Mateos <h.rodicio@hotmail.com>
2022-10-13 12:01:54 +0200

    Arreglado el conflicto

--- test_arduino_code.cpp
+++ test_arduino_code.cpp
@@ -6,97 +6,69 @@
 #include <stdio.h>
 
 extern "C" {
-#include "i386_code.h"
+#include "arduino_code.h"
 }
 
 /**********************************************************
- *  Test: control_temperature -> basic
+ *  Test: get_temperature
  *********************************************************/
-
-TEST(test_control_temperature, basic) 
+/*
+TEST(test_get_temperature, temp_fetch) 
 { 
     // test 1
-    temperature = 20;
-    control_temperature();
+    // test 2
+}
+*/
+
+TEST(test_exec_cmd_msg, basic) 
+{ 
+    // test 1
+    last_cmd_msg.cmd = 1;
+    last_cmd_msg.set_heater = 1;
+    exec_cmd_msg();
     ASSERT_EQ(1, heater_on);
-
-    // test 1
-    temperature = 60;
-    control_temperature();
-    ASSERT_EQ(0, heater_on);
-}
-
-/**********************************************************
- *  Test: send_cmd_msg -> heater
- *********************************************************/
-TEST(test_send_cmd_msg, heater) 
-{ 
-    // test 1
-    enum command cmd = SET_HEAT_CMD;
-    heater_on = 1;
-    send_cmd_msg(cmd);
-    ASSERT_EQ(1, next_cmd_msg.cmd);
-}
-
-
-/**********************************************************
- *  Test: recv_res_msg -> errors
- *********************************************************/
-
-TEST(test_recv_res_msg, errors) 
-{ 
-
-    // test 1
-    last_res_msg.cmd = 2;
-    last_res_msg.data.sunlight_on = 1;
-    recv_res_msg();
-
-    ASSERT_EQ(1, sunlight_on);
-    ASSERT_EQ(0, last_res_msg.status);
-    ASSERT_EQ(0, last_res_msg.cmd);
-
+    ASSERT_EQ(1, next_res_msg.cmd);
+    ASSERT_EQ(1, next_res_msg.status);
+    ASSERT_EQ(0, last_cmd_msg.cmd);
+    ASSERT_EQ(0, last_cmd_msg.set_heater);
 
     // test 2
-    last_res_msg.cmd = 3;
-    last_res_msg.data.temperature = 10;
-    recv_res_msg();
-    ASSERT_EQ(10, temperature);
-    ASSERT_EQ(0, last_res_msg.status);
-    ASSERT_EQ(0, last_res_msg.cmd);
-
+    last_cmd_msg.cmd = 2;
+    sunlight_on = 1;
+    exec_cmd_msg();
+    ASSERT_EQ(1, next_res_msg.data.sunlight_on);
+    ASSERT_EQ(2, next_res_msg.cmd);
+    ASSERT_EQ(1, next_res_msg.status);
+    ASSERT_EQ(0, last_cmd_msg.cmd);
+    ASSERT_EQ(0, last_cmd_msg.set_heater);
 
     // test 3
-    last_res_msg.cmd = 4;
-    last_res_msg.data.position = {2427.050983124840, 3526.711513754840, 9708.203932499370};
-    recv_res_msg();
-    EXPECT_NEAR(2427.050983124840, position.x, 0.001);
-    EXPECT_NEAR(3526.711513754840, position.y,0.001);
-    EXPECT_NEAR(9708.203932499370, position.z,0.001);
-    ASSERT_EQ(0, last_res_msg.status);
-    ASSERT_EQ(0, last_res_msg.cmd);
+    last_cmd_msg.cmd = 3;
+    temperature = 10;
+    exec_cmd_msg();
+    ASSERT_EQ(10, next_res_msg.data.temperature);
+    ASSERT_EQ(3, next_res_msg.cmd);
+    ASSERT_EQ(1, next_res_msg.status);
+    ASSERT_EQ(0, last_cmd_msg.cmd);
+    ASSERT_EQ(0, last_cmd_msg.set_heater);
 
-	
     // test 4
-    last_res_msg.cmd = 0;
-    recv_res_msg();
-    ASSERT_EQ(0, last_res_msg.cmd);
-    ASSERT_EQ(0, last_res_msg.status);
-	
-    // test 5
-    last_res_msg.cmd = 1;
-    recv_res_msg();
-    ASSERT_EQ(0, last_res_msg.cmd);
-    ASSERT_EQ(0, last_res_msg.status);
+    last_cmd_msg.cmd = 4;
+    position = {2427.050983124840, 3526.711513754840, 9708.203932499370};
+    exec_cmd_msg();
+    EXPECT_NEAR(2427.050983124840, next_res_msg.data.position.x, 0.001);
+    EXPECT_NEAR(3526.711513754840, next_res_msg.data.position.y, 0.001);
+    EXPECT_NEAR(9708.203932499370, next_res_msg.data.position.z, 0.001);
+    
+    ASSERT_EQ(4, next_res_msg.cmd);
+    ASSERT_EQ(1, next_res_msg.status);
+    ASSERT_EQ(0, last_cmd_msg.cmd);
+    ASSERT_EQ(0, last_cmd_msg.set_heater);
 }
-
-
-
-/**********************************************************
- *  Funtion: main
- *********************************************************/
 
 int main(int argc, char **argv) 
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
